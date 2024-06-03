667dd8753d095cb55401f2ccc6f0c4479ed815ec
Luckie Mumba <itxluckym@gmail.com>
2024-06-04 01:26:00 +0200

    modified:   index.html 	modified:   package.json 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: [Roboto],
-      },
+      // fontFamily: {
+      //   sans: [Roboto],
+      // },
       gridTemplateColumns: {
         "70-30": "70% 28%",
       },
