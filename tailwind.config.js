d9b9c9f6c6b597c7a4104644997ce1da1e8b0355
Luckie Mumba <itxluckym@gmail.com>
2024-06-07 00:39:32 +0200

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 // import acorn from "./src/assets/fonts/acorn.woff";
 // import gt from "./src/assets/fonts/gt.woff";
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
 
-      gridTemplateColumns: {
-        "70-30": "70% 28%",
-      },
-    },
-  },
-  plugins: [],
-};
+//       gridTemplateColumns: {
+//         "70-30": "70% 28%",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
